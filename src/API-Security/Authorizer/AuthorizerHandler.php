23c39f1d99eb3300f0c2179b8f50c039533cbbcd
Deverror 6068 <113009666+deverror6068@users.noreply.github.com>
2024-06-30 23:32:09 +0200

    modification

--- src/API-Security/Authorizer/AuthorizerHandler.php
+++ src/API-Security/Authorizer/AuthorizerHandler.php
@@ -21,39 +21,45 @@ class AuthorizerHandler
 
 
     static   function    CheckToken($token) :string{
-        ;
+        // print_r(json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[2])))));
+       // $secretkey = new \Authenticator\SecretKey("key.json");
         $secretkeyvalue = \SecurityLib::GetSecretKey("key.json");
-
+        var_dump($secretkeyvalue);
+        var_dump($token);
         $secretkeyvalue = json_decode(json_encode($secretkeyvalue,true));
-
+        var_dump((array)$secretkeyvalue);
         $secretkeyvalue = (array($secretkeyvalue));
         $secretkeyvalue = (implode("/", (array)$secretkeyvalue[0]));
         $secretkeyvalue= (explode("/",$secretkeyvalue));
         $secretkeyvalue = $secretkeyvalue[1];
-
-
-        if(isset($_COOKIE["identification"])){
-            $cipheredtoken = $_COOKIE["identification"] ;
-        }else{
-            $_COOKIE["identification"] = null;
-        }
-
-
+        var_dump($secretkeyvalue);
+       // return  $secretkeyvalue;
+        $cipheredtoken = $_COOKIE["identification"] ;
+       // $explode = explode(".",$cipheredtoken["identification"]);
+       // var_dump($explode);
+        var_dump($_COOKIE);
         $ciphermethod = new \Authenticator\CipherLib();
         $uncipheredtoken = \Authenticator\CipherLib::UncipherToken($cipheredtoken);
         $header =  $uncipheredtoken['header'];
         $payload =  $uncipheredtoken['payload'];
 
-
+        var_dump($uncipheredtoken);
+        var_dump($uncipheredtoken['signature']);
+        var_dump(base64_decode($secretkeyvalue));
 
 
         $signature = hash_hmac('sha256',  base64_encode(json_encode($header))   . "." . base64_encode(json_encode($payload)), $secretkeyvalue, true);
        // $signature = base64_encode($signature);
         $expectedsignature = str_replace(['+', '/', '='], ['-', '_', ''],base64_encode($signature));
-
+        var_dump($signature);
 
        // var_dump($signature);
-
+        var_dump(json_encode(['typ' => 'JWT', 'alg' => 'HS256']));
+        var_dump(json_encode($uncipheredtoken["header"]));
+        var_dump($expectedsignature);
+        var_dump($uncipheredtoken['signature']);
+        var_dump($signature);
+        var_dump($uncipheredtoken["header"]['typ']==="JWT");
 
        // var_dump();
 
@@ -74,14 +80,14 @@ class AuthorizerHandler
 
 
         if($payload['iat']<time()){
-            echo  "<p> token expired please log at /login </p>";
+            echo  "<p> token expired please login at /login </p>";
             $res = "expired";
             return $res;
 
         }else{
-
+            echo "connexion succeed";
             $res = "success";
-
+            var_dump($_COOKIE);
             return  $res;
         }
 
@@ -107,11 +113,13 @@ class AuthorizerHandler
 static function  ClearToken(): string
 {
     $cipheredtoken = explode(".",$_COOKIE["identification"]);
-
+    var_dump($_COOKIE);
+    var_dump($cipheredtoken[1]);
     setcookie("identification",$cipheredtoken[1],time()-360000,"/");
     //$_COOKIE = "" ;
-    echo "deconnexion réussie <br>";
-    echo "allez sur la page /Login pour se Connecter";
+
+
+            var_dump($_COOKIE);
     return "cookies cleared";
 }
 
--- src/API-Security/Login/index.php
+++ src/API-Security/Login/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/API-Security/Service/LoginPage.php");
 require_once($_SERVER["DOCUMENT_ROOT"] . "/API-Security/Service/Service.php");
-echo "nuldl";
+
 LoginPage::Endpoint();
