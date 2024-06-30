ede780e56b0976415fce8ade13336169017764b4
Deverror 6068 <113009666+deverror6068@users.noreply.github.com>
2024-06-30 23:06:47 +0200

    correction mineure

--- src/API-Security/Register/index.php
+++ src/API-Security/Register/index.php
@@ -2,5 +2,5 @@
 
 require_once($_SERVER["DOCUMENT_ROOT"] . "/API-Security/Service/Register.php");
 require_once($_SERVER["DOCUMENT_ROOT"] . "/API-Security/Service/Service.php");
-echo "nuldl";
+
 Register::Endpoint();
